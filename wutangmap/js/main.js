4f194dcb1ccc25502163032a118b5f43932b5a57
Katie Kowalsky <kkowalsky@wisc.edu>
2015-05-07 14:40:12 -0500

    update map frame + tileset

--- wutangmap/js/main.js
+++ wutangmap/js/main.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
-    //declare global variables
     var cities;
-    //create a leaflet map with these variables
-    var map = L.map('map', {
-                center: [39.0, 25.7],
-                zoom: 2,
-        });
-    //add stamen toner basemap to map
-        L.tileLayer(
-        'http://a.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-                    attribution: '<a href="http://maps.stamen.com">Stamen tileset</a>'
-        }).addTo(map);
+    var map = L.map('map').setView([39.0, 25.7],2);
+    
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+}).addTo(map);
 
-    //push geojson data to load and create different map components
-    $.getJSON("data/wutangshapes.geojson")
-        .done(function(data) {
-        })
-    .fail(function() { alert("There has been a problem loading the data")});
-});//end main.js
+}); //end main.js
 
 
