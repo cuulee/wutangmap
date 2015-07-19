118947d528b18d7a1f23b6dec6f04def491ba063
Katie Kowalsky <kkowalsky@wisc.edu>
2015-07-19 16:28:06 -0400

    loads in JSON shapes

--- wutangmap/js/main.js
+++ wutangmap/js/main.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){ 
     var cities;
     var map = L.map('map').setView([39.0, 25.7],2);
+    var shapes = new L.geoJson();
     
     L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 }).addTo(map);
 
+    shapes.addTo(map);
+
+    $.ajax({
+        dataType: "json",
+        url: "data/wutang_shapes.geojson",
+        success: function(data) {
+            $(data.features).each(function(key, data) {
+                shapes.addData(data);
+                console.log(key)
+                console.log(data.properties.album)
+            });
+        }
+    }).error(function() {});
     //push geojson data to load and create different map components
-    $.getJSON("data/wutang_shapes.geojson")
+    /*$.getJSON("data/wutang_shapes.geojson")
         .done(function(data) {
             var info = processData(data);
             createIcons(data);
@@ -17,8 +31,7 @@ $(document).ready(function(){
 
     function processData(data){
         console.log(data);
-        
-    }; //end processData
+    }; //end processData*/
     
     function createIcons(data){
         var wuIcon = L.icon({
